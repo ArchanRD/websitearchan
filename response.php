03ee7bd4684527e292815895d5c69a0519cdc615
ArchanRD <archanrd29@gmail.com>
2022-01-07 17:07:44 +0530

    new

---
+++ response.php
@@ -0,0 +1,1 @@
+    echo Your response is submitted
---
+++ tut.java
@@ -0,0 +1,13 @@
+
+
+class multi {
+    public static void main(String[] args) {
+    
+        for(int i=1; i<=10; i++){
+            for(int j=1; i<=10; j++){
+                System.out.println(i + "*" + j + "=" + (i*j))
+            }
+        }
+    
+    }
+}
